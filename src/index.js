75ac644ca4758033f39c45b95983d1a4463c4dbc
yessimov-a <51717496+yessimov-a@users.noreply.github.com>
2020-06-22 23:08:41 +0600

    fix: import compose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import { App } from './App'
 import { rootReducer } from './reducers'
